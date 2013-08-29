1dd75b5c3ee1eb80dfdcaa2d5bd743cb470b36fa
Jens Diemer <github.com@jensdiemer.de>
2013-08-29 09:14:54 +0200

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,3 @@
-
 """
     DragonPy - Dragon 32 emulator in Python
     =======================================
@@ -10,12 +9,12 @@
 
 class CfgDragon32(object):
     """
-    Der maximal von der CPU adressierbare Speicherbereich von 64 kB (hexadezimal: $FFFF Bytes) unterteilt sich beim
-    Dragon 32 im Wesentlichen in vier verschiedene Sektionen:
-    32 kB RAM ($0000-$7FFF),
-    16 kB ROM ($8000-$BFFF), ein etwa
-    16 kB umfassender freier Speicherblock ($C000-$FEFF) und
-    Sonderspeicher mit u.a. Hardwareregistern des 6883-SAM und der beiden PIAs ($FF00-$FFFF).
+    max Memory is 64KB ($FFFF Bytes)
+    
+    32 kB RAM ($0000-$7FFF)
+    16 kB ROM ($8000-$BFFF)
+    ~16 kB free/reseved ($C000-$FEFF)
+    $FF00-$FFFF 6883-SAM / PIA
     """
     RAM_START = 0x0000
     RAM_SIZE = 0x7FFF
