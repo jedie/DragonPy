ab16f3c44e9da6006f90ef2db2459542cecf1efc
JensDiemer <git@jensdiemer.de>
2013-09-23 12:07:53 +0200

    reimplement skeleton maker script

--- 6809data/CPU6809Instructions_build_skeleton.py
+++ 6809data/CPU6809Instructions_build_skeleton.py
@@ -5,75 +5,97 @@
     data from:
         * http://www.maddes.net/m6809pm/sections.htm#sec4_4
         * http://www.burgins.com/m6809.html
+        * http://www.maddes.net/m6809pm/appendix_a.htm#appA
 
     :copyleft: 2013 by Jens Diemer
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-from CPU6809Instructions import ADDRES_MODE_DICT, OP_CATEGORIES, OP_DATA, OPERANT_DICT
+
 import pprint
+import sys
 
-# def get_group_by_category(data):
-#     result = {}
-#     for opcode, op_data in data.items():
-#         category_id = op_data["category"]
-#         op_data["opcode"] = opcode
-#         result.setdefault(category_id, []).append(op_data)
-#     return result
-#
-# op_data = get_group_by_category(OP_DATA)
+from MC6809_data_raw import OP_CATEGORIES, INSTRUCTION_INFO, OP_DATA, ADDRES_MODE_DICT
 
-print "class CPU6809(object):"
 
-data = {}
-for op_data in OP_DATA:
-#     print op_data
-    category = op_data.pop("category")
-    data.setdefault(category, {})
-    category_dict = data[category]
+class Tee(object):
+    def __init__(self, filepath, origin_out):
+        self.filepath = filepath
+        self.origin_out = origin_out
+        self.f = file(filepath, "w")
 
-    instruction = op_data.pop("instruction")
-    category_dict.setdefault(instruction, {})
-    instruction_dict = category_dict[instruction]
+    def write(self, *args):
+        txt = " ".join(args)
+        self.origin_out.write(txt)
+        self.f.write(txt)
 
-    instruction_dict["desc"] = op_data.pop("desc")
+    def close(self):
+        self.f.close()
+        sys.stdout = self.origin_out
 
-    opcode = op_data.pop("opcode")
-    opcode_dict = instruction_dict.setdefault(opcode, {})
-    opcode_dict.update(op_data)
 
-# pprint.pprint(data)
+sys.stdout = Tee("MC6809_skeleton.py", sys.stdout)
 
-for category_id, category_data in sorted(data.items()):
-    print
-    print "    #### %s" % OP_CATEGORIES[category_id]
-    print
-    for instruction, instruction_data in sorted(category_data.items()):
-        desc = instruction_data.pop("desc")
-        if desc:
-            print '    # %s - %s' % (instruction, desc)
+
+print '"""%s"""' % __doc__
+print
+print "def opcode(): raise NotImplementedError"
+print
+print "class CPU6809Skeleton(object):"
+
+
+added_ops = []
+# very ineffective, but hey, must only run one time ;)
+for instr_key, instr_data in sorted(INSTRUCTION_INFO.items()):
+    ops = []
+    for op_data in OP_DATA:
+        if op_data["instr_info_key"] == instr_key:
+            ops.append(op_data)
+
+    if not ops:
+        continue
+
+    op_info = {}
+    for op in ops:
+        mnemonic = op["mnemonic"]
+        if mnemonic not in op_info:
+            op_info[mnemonic] = {"opcodes": [op["opcode"]], "addr_modes": [op["addr_mode"]]}
         else:
-            print '    # %s' % instruction
+            op_info[mnemonic]["opcodes"].append(op["opcode"])
+            op_info[mnemonic]["addr_modes"].append(op["addr_mode"])
+
+    if "short_desc" in instr_data:
+        print '    @opcode(( # %s' % instr_data["short_desc"]
+    else:
         print '    @opcode(('
-        for opcode, op_data in sorted(instruction_data.items()):
-            args = [
-                "0x%x" % opcode,
-                '"%s"' % op_data["mnemonic"],
-                ADDRES_MODE_DICT[op_data["addr_mode"]],
-            ]
-            operant = op_data["operant"]
-            if operant is not None:
-#                 args.append("operant=%s # %s" % (
-#                     operant, OPERANT_DICT[operant]
-#                 ))
-                args.append("operant=%s" % operant)
 
-            print "        %s" % ", ".join(args)
+    for mnemonic, ops in sorted(op_info.items()):
+        line = "        "
+        opcodes = []
+        for opcode in ops["opcodes"]:
+            assert opcode not in added_ops, repr(op)
+            opcodes.append(opcode)
+        line += "".join(["0x%x, " % i for i in opcodes])
+        line += "# %s (" % mnemonic
+        line += ", ".join(
+            [ADDRES_MODE_DICT[addr_mode].lower() for addr_mode in ops["addr_modes"]]
+        )
+        line += ")"
+        print line
 
-        print "    ))"
-        print "    def %s(self, op, ea=None, operant=None):" % instruction
-#         if desc:
-#             print '        """'
-#             print '        %s' % desc
-#             print '        """'
-        print '        raise NotImplementedError("TODO: $%%x %s") %% op' % instruction
-        print
+    print '    ))'
+    print "    def op_%s(self, op, ea=None, operant=None):" % instr_key
+    print '        """'
+    for line in instr_data.get("description", "").split("\n"):
+        print '        %s' % line
+    for line in instr_data.get("comment", "").split("\n"):
+        print '        %s' % line
+    for line in instr_data.get("source form", "").split("\n"):
+        print '        %s' % line
+    print '        """'
+    print '        raise NotImplementedError("TODO: $%%x %s") %% op' % instr_key
+    print
+
+
+sys.stdout.close()
+print "%i opcodes" % len(added_ops)
+
