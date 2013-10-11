9591467add6c274847f168f0468d08fc91714094
JensDiemer <git@jensdiemer.de>
2013-10-11 22:41:09 +0200

    better opcode .csc export output

--- MC6809data/CPU6809csv_export.py
+++ MC6809data/CPU6809csv_export.py
@@ -14,34 +14,52 @@
 import pprint
 import sys
 
-from MC6809_data_raw import OP_CATEGORIES, INSTRUCTION_INFO, OP_DATA, ADDRES_MODE_DICT
+from MC6809_data_raw import INSTRUCTION_INFO, OP_DATA
+from MC6809data.MC6809_data_raw import MEM_ACCESS_BYTE, MEM_ACCESS_WORD
 
 
 keys = OP_DATA[0].keys()
 keys.insert(3, "opcode_hex")
 
+FROM_INSTRUCTION_INFO = ['HNZVC', 'instr_desc']
+
+
+MEM_ACCESS_MAP = {
+    MEM_ACCESS_BYTE: "byte",
+    MEM_ACCESS_WORD: "word",
+}
+
+
 import csv
 with open('CPU6809_opcodes.csv', 'wb') as csvfile:
     w = csv.writer(csvfile,
-        delimiter=';',
+        delimiter='\t',
         quotechar='|', quoting=csv.QUOTE_MINIMAL
     )
+
+    w.writerow(keys + FROM_INSTRUCTION_INFO)
 
     for op_data in OP_DATA:
         row = []
 
-        op_data["bytes"] = "%i bytes" % op_data["bytes"]
         op_data["opcode_hex"] = hex(op_data["opcode"])
 
-        for k in keys:
-            data = op_data[k]
+        for key in keys:
+            data = op_data.get(key, "-")
+            if key == "mem_access" and data != "-":
+                data = MEM_ACCESS_MAP[data]
+                
+            if isinstance(data, basestring):
+                data = data.replace("\t", "    ")
             row.append(data)
-            
+
         instr_info_key = op_data["instr_info_key"]
-        row.append(
-            INSTRUCTION_INFO[instr_info_key].get('operation', "").replace("\n", " ").replace(";", "|")
-        )
-            
+        instr_info = INSTRUCTION_INFO[instr_info_key]
+        for key in FROM_INSTRUCTION_INFO:
+            row.append(
+                instr_info.get(key, "").replace("\n", " ").replace("\t", "    ")
+            )
+
         print row
         w.writerow(row)
 
