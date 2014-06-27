c7d39ffc2e1a410b0cef3b462aa8fbfaa0a4150e
JensDiemer <git@jensdiemer.de>
2014-06-27 18:27:03 +0200

    update accu unittests

--- tests/test_accumulators.py
+++ tests/test_accumulators.py
@@ -4,57 +4,47 @@
     DragonPy - Dragon 32 emulator in Python
     =======================================
 
-    6809 is Big-Endian
-    some code is from XRoar emulator by Ciaran Anscomb (GPL license) more info, see README
-
-    :copyleft: 2013 by the DragonPy team, see AUTHORS for more details.
+    :copyleft: 2013-2014 by the DragonPy team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
 import unittest
 
-from test_base import BaseTestCase, TestCPU
+from test_base import BaseTestCase
 
 
 class CC_AccumulatorTestCase(BaseTestCase):
-    def setUp(self):
-        self.cpu = TestCPU()
+    def test_A_01(self):
+        self.cpu.accu_a.set(0xff)
+        self.assertEqualHex(self.cpu.accu_a.get(), 0xff)
 
-    def test_A01(self):
-        self.cpu.accu_a = 0xff
-        self.assertEqualHex(self.cpu.accu_a, 0xff)
+    def test_A_02(self):
+        self.cpu.accu_a.set(0xff + 1)
+        self.assertEqualHex(self.cpu.accu_a.get(), 0x00)
+
+    def test_B_01(self):
+        self.cpu.accu_b.set(0x5a)
+        self.assertEqualHex(self.cpu.accu_b.get(), 0x5a)
         self.assertEqual(self.cpu.cc.V, 0)
 
-        self.cpu.accu_a = 0xff + 1
-        self.assertEqual(self.cpu.cc.V, 1)
-        self.assertEqualHex(self.cpu.accu_a, 0x0) # XXX
+    def test_B_02(self):
+        self.cpu.accu_b.set(0xff + 10)
+        self.assertEqualHex(self.cpu.accu_b.get(), 0x09)
 
-    def test_B01(self):
-        self.cpu.accu_b = 0x5a
-        self.assertEqualHex(self.cpu.accu_b, 0x5a)
-        self.assertEqual(self.cpu.cc.V, 0)
+    def test_D_01(self):
+        self.cpu.accu_a.set(0x12)
+        self.cpu.accu_b.set(0xab)
+        self.assertEqualHex(self.cpu.accu_d.get(), 0x12ab)
 
-        self.cpu.accu_b = 0xff + 10
-        self.assertEqual(self.cpu.cc.V, 1)
-        self.assertEqualHex(self.cpu.accu_b, 0x9) # XXX
+    def test_D_02(self):
+        self.cpu.accu_d.set(0xfd89)
+        self.assertEqualHex(self.cpu.accu_a.get(), 0xfd)
+        self.assertEqualHex(self.cpu.accu_b.get(), 0x89)
 
-    def test_D01(self):
-        self.cpu.accu_a = 0x12
-        self.cpu.accu_b = 0xab
-        self.assertEqualHex(self.cpu.accu.D, 0x12ab)
-        self.assertEqual(self.cpu.cc.V, 0)
-
-    def test_D02(self):
-        self.cpu.accu.D = 0xfd89
-        self.assertEqualHex(self.cpu.accu_a, 0xfd)
-        self.assertEqualHex(self.cpu.accu_b, 0x89)
-        self.assertEqual(self.cpu.cc.V, 0)
-
-    def test_D03(self):
-        self.cpu.accu.D = 0xffff + 1
-        self.assertEqualHex(self.cpu.accu_a, 0x0) # XXX
-        self.assertEqualHex(self.cpu.accu_b, 0x0) # XXX
-        self.assertEqual(self.cpu.cc.V, 1)
+    def test_D_03(self):
+        self.cpu.accu_d.set(0xffff + 1)
+        self.assertEqualHex(self.cpu.accu_a.get(), 0x00)
+        self.assertEqualHex(self.cpu.accu_b.get(), 0x00)
 
 
 if __name__ == '__main__':
