358dbc87f1dd3d247b43fba2aa605e9689c5bc54
JensDiemer <git@jensdiemer.de>
2014-06-27 18:42:37 +0200

    Add complete range unittests for update_HNZVC_8

--- tests/test_condition_code_register.py
+++ tests/test_condition_code_register.py
@@ -4,15 +4,15 @@
     DragonPy - Dragon 32 emulator in Python
     =======================================
 
-    :copyleft: 2013 by the DragonPy team, see AUTHORS for more details.
+    :copyleft: 2013-2014 by the DragonPy team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
 import unittest
-
-from test_base import BaseTestCase, TestCPU
 import logging
 import sys
+
+from test_base import BaseTestCase
 from tests.test_base import TextTestRunner2
 from cpu_utils.MC6809_registers import signed8
 
@@ -24,78 +24,101 @@ class CCTestCase(BaseTestCase):
             status_byte = self.cpu.cc.get()
             self.assertEqual(status_byte, i)
 
-    def test_HalfCarry(self):
-        half_carry_must_set = range(0, 255, 16)
-        for i in xrange(256):
+    def test_HNZVC_8(self):
+        for i in xrange(280):
             self.cpu.cc.set(0x00)
-            self.cpu.cc.update_HNZVC_8(a=0, b=i, r=i) # e.g.: 0+2=2
-            a = 0
-            b = i
-            r = i
-            print i, self.cpu.cc.H, (0b00010000 & i) >> 5
-#             if i in half_carry_must_set:
-#                 self.assertEqual(self.cpu.cc.H, 1, "Error in %i" % i)
-#             else:
-#                 self.assertEqual(self.cpu.cc.H, 0, "Error in %i" % i)
+            r = i + 1 # e.g. ADDA 1 loop
+            self.cpu.cc.update_HNZVC_8(a=i, b=1, r=r)
+            #print r, self.cpu.cc.get_info
 
-    def test_update_NZ_8(self):
+            # test half carry
+            if r % 16 == 0:
+                self.assertEqual(self.cpu.cc.H, 1)
+            else:
+                self.assertEqual(self.cpu.cc.H, 0)
+
+            # test negative
+            if 128 <= r <= 255:
+                self.assertEqual(self.cpu.cc.N, 1)
+            else:
+                self.assertEqual(self.cpu.cc.N, 0)
+
+            # test zero
+            if signed8(r) == 0:
+                self.assertEqual(self.cpu.cc.Z, 1)
+            else:
+                self.assertEqual(self.cpu.cc.Z, 0)
+
+            # test overflow
+            if r == 128 or r > 256:
+                self.assertEqual(self.cpu.cc.V, 1)
+            else:
+                self.assertEqual(self.cpu.cc.V, 0)
+
+            # test carry
+            if r > 255:
+                self.assertEqual(self.cpu.cc.C, 1)
+            else:
+                self.assertEqual(self.cpu.cc.C, 0)
+
+            # Test that CC registers doesn't reset automaticly
+            self.cpu.cc.set(0xff)
+            r = i + 1 # e.g. ADDA 1 loop
+            self.cpu.cc.update_HNZVC_8(a=i, b=1, r=r)
+            #print "+++", r, self.cpu.cc.get_info
+            self.assertEqualHex(self.cpu.cc.get(), 0xff)
+
+
+    def test_update_NZ_8_A(self):
         self.cpu.cc.update_NZ_8(r=0x12)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 0)
 
+    def test_update_NZ_8_B(self):
         self.cpu.cc.update_NZ_8(r=0x0)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 1)
 
+    def test_update_NZ_8_C(self):
         self.cpu.cc.update_NZ_8(r=0x80)
         self.assertEqual(self.cpu.cc.N, 1)
         self.assertEqual(self.cpu.cc.Z, 0)
 
-    def test_update_NZ0_16(self):
+    def test_update_NZ0_16_A(self):
         self.cpu.cc.update_NZ0_16(r=0x7fff) # 0x7fff == 32767
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 0)
         self.assertEqual(self.cpu.cc.V, 0)
 
+    def test_update_NZ0_16_B(self):
         self.cpu.cc.update_NZ0_16(r=0x00)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 1)
         self.assertEqual(self.cpu.cc.V, 0)
 
+    def test_update_NZ0_16_C(self):
         self.cpu.cc.update_NZ0_16(r=0x8000) # signed 0x8000 == -32768
         self.assertEqual(self.cpu.cc.N, 1)
         self.assertEqual(self.cpu.cc.Z, 0)
         self.assertEqual(self.cpu.cc.V, 0)
 
-    def test_update_NZ0_8(self):
+    def test_update_NZ0_8_A(self):
         self.cpu.cc.update_NZ0_8(0x7f)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 0)
         self.assertEqual(self.cpu.cc.V, 0)
 
+    def test_update_NZ0_8_B(self):
         self.cpu.cc.update_NZ0_8(0x100)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 1)
-        self.assertEqual(self.cpu.cc.V, 1)
-
-    def test_update_NZVC_8(self):
-        a = 1
-        b = 2
-        r = a + b
-        self.cpu.cc.update_NZVC_8(a, b, r)
-        self.assertEqual(self.cpu.cc.N, 0)
-        self.assertEqual(self.cpu.cc.Z, 0)
         self.assertEqual(self.cpu.cc.V, 0)
-        self.assertEqual(self.cpu.cc.C, 0)
 
-        a = 0xff
-        b = 1
-        r = a + b
-        self.cpu.cc.update_NZVC_8(a, b, r)
+    def test_update_NZV_8_B(self):
+        self.cpu.cc.update_NZ0_8(0x100)
         self.assertEqual(self.cpu.cc.N, 0)
         self.assertEqual(self.cpu.cc.Z, 1)
-        self.assertEqual(self.cpu.cc.V, 1) # FIXME
-        self.assertEqual(self.cpu.cc.C, 0)
+        self.assertEqual(self.cpu.cc.V, 0)
 
 
 if __name__ == '__main__':
