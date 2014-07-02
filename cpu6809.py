c7428bd42c7b5454a7cf505e5ea2c7a6797cde56
JensDiemer <git@jensdiemer.de>
2014-07-02 09:37:04 +0200

    group ST/LD and add unittests

--- cpu6809.py
+++ cpu6809.py
@@ -1475,52 +1475,6 @@ class CPU(object):
         self.program_counter = ea
 
 
-
-    @opcode(# Load register from memory
-        0xcc, 0xdc, 0xec, 0xfc, # LDD (immediate, direct, indexed, extended)
-        0x10ce, 0x10de, 0x10ee, 0x10fe, # LDS (immediate, direct, indexed, extended)
-        0xce, 0xde, 0xee, 0xfe, # LDU (immediate, direct, indexed, extended)
-        0x8e, 0x9e, 0xae, 0xbe, # LDX (immediate, direct, indexed, extended)
-        0x108e, 0x109e, 0x10ae, 0x10be, # LDY (immediate, direct, indexed, extended)
-    )
-    def instruction_LD16(self, opcode, m, register):
-        """
-        Load the contents of the memory location M:M+1 into the designated
-        16-bit register.
-
-        source code forms: LDD P; LDX P; LDY P; LDS P; LDU P
-
-        CC bits "HNZVC": -aa0-
-        """
-#        log.debug("$%x LD16 set %s to $%x \t| %s" % (
-#            self.program_counter,
-#            register.name, m,
-#            self.cfg.mem_info.get_shortest(m)
-#        ))
-        register.set(m)
-        self.cc.clear_NZV()
-        self.cc.update_NZ_16(m)
-
-    @opcode(# Load accumulator from memory
-        0x86, 0x96, 0xa6, 0xb6, # LDA (immediate, direct, indexed, extended)
-        0xc6, 0xd6, 0xe6, 0xf6, # LDB (immediate, direct, indexed, extended)
-    )
-    def instruction_LD8(self, opcode, m, register):
-        """
-        Loads the contents of memory location M into the designated register.
-
-        source code forms: LDA P; LDB P
-
-        CC bits "HNZVC": -aa0-
-        """
-#        log.debug("$%x LD8 %s = $%x" % (
-#            self.program_counter,
-#            register.name, m,
-#        ))
-        register.set(m)
-        self.cc.clear_NZV()
-        self.cc.update_NZ_8(m)
-
     @opcode(# Load effective address into an indexable register
         0x32, # LEAS (indexed)
         0x33, # LEAU (indexed)
@@ -1822,53 +1776,7 @@ class CPU(object):
         self.cc.clear_NZ()
         self.cc.update_NZ_16(d)
 
-    @opcode(# Store register to memory
-        0xdd, 0xed, 0xfd, # STD (direct, indexed, extended)
-        0x10df, 0x10ef, 0x10ff, # STS (direct, indexed, extended)
-        0xdf, 0xef, 0xff, # STU (direct, indexed, extended)
-        0x9f, 0xaf, 0xbf, # STX (direct, indexed, extended)
-        0x109f, 0x10af, 0x10bf, # STY (direct, indexed, extended)
-    )
-    def instruction_ST16(self, opcode, ea, register):
-        """
-        Writes the contents of a 16-bit register into two consecutive memory
-        locations.
 
-        source code forms: STD P; STX P; STY P; STS P; STU P
-
-        CC bits "HNZVC": -aa0-
-        """
-        value = register.get()
-#        log.debug("$%x ST16 store value $%x from %s at $%x \t| %s" % (
-#            self.program_counter,
-#            value, register.name, ea,
-#            self.cfg.mem_info.get_shortest(ea)
-#        ))
-        self.cc.clear_NZV()
-        self.cc.update_NZ_16(value)
-        return ea, value
-
-    @opcode(# Store accumulator to memroy
-        0x97, 0xa7, 0xb7, # STA (direct, indexed, extended)
-        0xd7, 0xe7, 0xf7, # STB (direct, indexed, extended)
-    )
-    def instruction_ST8(self, opcode, ea, register):
-        """
-        Writes the contents of an 8-bit register into a memory location.
-
-        source code forms: STA P; STB P
-
-        CC bits "HNZVC": -aa0-
-        """
-        value = register.get()
-#        log.debug("$%x ST8 store value $%x from %s at $%x \t| %s" % (
-#            self.program_counter,
-#            value, register.name, ea,
-#            self.cfg.mem_info.get_shortest(ea)
-#        ))
-        self.cc.clear_NZV()
-        self.cc.update_NZ_8(value)
-        return ea, value
 
     @opcode(# Subtract memory from accumulator
         0x80, 0x90, 0xa0, 0xb0, # SUBA (immediate, direct, indexed, extended)
@@ -1932,6 +1840,103 @@ class CPU(object):
         # TODO: check if source and dest has the same size?
         source = self.get_register(high)
         self.set_register(low, source)
+
+
+    # ---- Store / Load ----
+
+
+    @opcode(# Load register from memory
+        0xcc, 0xdc, 0xec, 0xfc, # LDD (immediate, direct, indexed, extended)
+        0x10ce, 0x10de, 0x10ee, 0x10fe, # LDS (immediate, direct, indexed, extended)
+        0xce, 0xde, 0xee, 0xfe, # LDU (immediate, direct, indexed, extended)
+        0x8e, 0x9e, 0xae, 0xbe, # LDX (immediate, direct, indexed, extended)
+        0x108e, 0x109e, 0x10ae, 0x10be, # LDY (immediate, direct, indexed, extended)
+    )
+    def instruction_LD16(self, opcode, m, register):
+        """
+        Load the contents of the memory location M:M+1 into the designated
+        16-bit register.
+
+        source code forms: LDD P; LDX P; LDY P; LDS P; LDU P
+
+        CC bits "HNZVC": -aa0-
+        """
+#        log.debug("$%x LD16 set %s to $%x \t| %s" % (
+#            self.program_counter,
+#            register.name, m,
+#            self.cfg.mem_info.get_shortest(m)
+#        ))
+        register.set(m)
+        self.cc.clear_NZV()
+        self.cc.update_NZ_16(m)
+
+    @opcode(# Load accumulator from memory
+        0x86, 0x96, 0xa6, 0xb6, # LDA (immediate, direct, indexed, extended)
+        0xc6, 0xd6, 0xe6, 0xf6, # LDB (immediate, direct, indexed, extended)
+    )
+    def instruction_LD8(self, opcode, m, register):
+        """
+        Loads the contents of memory location M into the designated register.
+
+        source code forms: LDA P; LDB P
+
+        CC bits "HNZVC": -aa0-
+        """
+#        log.debug("$%x LD8 %s = $%x" % (
+#            self.program_counter,
+#            register.name, m,
+#        ))
+        register.set(m)
+        self.cc.clear_NZV()
+        self.cc.update_NZ_8(m)
+
+    @opcode(# Store register to memory
+        0xdd, 0xed, 0xfd, # STD (direct, indexed, extended)
+        0x10df, 0x10ef, 0x10ff, # STS (direct, indexed, extended)
+        0xdf, 0xef, 0xff, # STU (direct, indexed, extended)
+        0x9f, 0xaf, 0xbf, # STX (direct, indexed, extended)
+        0x109f, 0x10af, 0x10bf, # STY (direct, indexed, extended)
+    )
+    def instruction_ST16(self, opcode, ea, register):
+        """
+        Writes the contents of a 16-bit register into two consecutive memory
+        locations.
+
+        source code forms: STD P; STX P; STY P; STS P; STU P
+
+        CC bits "HNZVC": -aa0-
+        """
+        value = register.get()
+        log.debug("$%x ST16 store value $%x from %s at $%x \t| %s" % (
+            self.program_counter,
+            value, register.name, ea,
+            self.cfg.mem_info.get_shortest(ea)
+        ))
+        self.cc.clear_NZV()
+        self.cc.update_NZ_16(value)
+        return ea, value
+
+    @opcode(# Store accumulator to memroy
+        0x97, 0xa7, 0xb7, # STA (direct, indexed, extended)
+        0xd7, 0xe7, 0xf7, # STB (direct, indexed, extended)
+    )
+    def instruction_ST8(self, opcode, ea, register):
+        """
+        Writes the contents of an 8-bit register into a memory location.
+
+        source code forms: STA P; STB P
+
+        CC bits "HNZVC": -aa0-
+        """
+        value = register.get()
+        log.debug("$%x ST8 store value $%x from %s at $%x \t| %s" % (
+            self.program_counter,
+            value, register.name, ea,
+            self.cfg.mem_info.get_shortest(ea)
+        ))
+        self.cc.clear_NZV()
+        self.cc.update_NZ_8(value)
+        return ea, value
 
 
     # ---- Logical Operations ----
