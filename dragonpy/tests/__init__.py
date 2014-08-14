6cb959e0f10108c6132da8c54a8672ac582fa28f
Jens Diemer <git@jensdiemer.de>
2014-08-14 22:38:22 +0200

    disable logging for run all unittests

---
+++ dragonpy/tests/__init__.py
@@ -0,0 +1,7 @@
+from dragonpy.utils.logging_utils import disable_logging, log
+
+# for running unittests via:
+#    python2 -m unittest discover
+# or with:
+#    ./run_all_tests.py
+disable_logging(log)
--- run_all_tests.py
+++ run_all_tests.py
@@ -10,25 +10,13 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-import unittest
 import logging
+import unittest
 
 from dragonpy.tests.test_base import TextTestRunner2
 
 
 if __name__ == '__main__':
-    log = logging.getLogger("DragonPy")
-    log.setLevel(
-#         1
-#         10 # DEBUG
-#         20 # INFO
-#         30 # WARNING
-#         40 # ERROR
-        50 # CRITICAL/FATAL
-    )
-    log.addHandler(logging.StreamHandler())
-
-
     loader = unittest.TestLoader()
     tests = loader.discover('.')
 
