ef8f151b7e9ccb305c6ac62581fe1e0a41071b11
JensDiemer <git@jensdiemer.de>
2014-09-03 18:10:25 +0200

    Update README

---
+++ dev_startup_scripts/run_PyPy2_benchmark_loop.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+title "%~0"
+
+set python=C:\pypy-2.3.1-win32\pypy.exe
+if NOT exist %python% (
+    echo Error: '%python%' doesn't exists?!?
+    pause
+    exit 1
+)
+
+cd..
+
+:loop
+    echo on
+    %python% -OO DragonPy_CLI.py benchmark
+    @echo off
+    pause
+goto:loop
