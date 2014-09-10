c3cb13a4638294ddeb145c87faa59b680b8f4de3
Jens Diemer <git@jensdiemer.de>
2014-09-10 16:40:50 +0200

    use --verbosity 99 in coco .sh, too

--- run_CoCo2b.sh
+++ run_CoCo2b.sh
@@ -4,11 +4,7 @@
 
 (
     set -x
-    #~ python2 DragonPy_CLI.py --machine=CoCo2b run | python ../PyDragon32/misc/filter_xroar_trace.py --display --start-stop=B3D9-ffff | tee CoCo2b_test_trace.txt
-    #~ python2 DragonPy_CLI.py --machine=CoCo2b run | python ../PyDragon32/misc/filter_xroar_trace.py  --unique | tee CoCo2b_test_trace.txt
-    #~ python2 DragonPy_CLI.py --machine=CoCo2b run > CoCo2b_test_trace.txt
-    #~ python2 DragonPy_CLI.py --machine=CoCo2b run | tee CoCo2b_test_trace.txt
-    python2 DragonPy_CLI.py --machine=CoCo2b run
+    python2 DragonPy_CLI.py  --verbosity 99 --machine=CoCo2b run
 )
 echo
 read -n1 -p "Start bash? [y,n]" doit
