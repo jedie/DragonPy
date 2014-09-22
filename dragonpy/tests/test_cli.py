160000808f0f2615dd7c95b8e37a8d15eaabf8b2
JensDiemer <git@jensdiemer.de>
2014-09-22 16:48:40 +0200

    update unittest

--- dragonpy/tests/test_cli.py
+++ dragonpy/tests/test_cli.py
@@ -70,7 +70,7 @@ class CLITestCase(unittest.TestCase):
 #        print(cli_err)
         self.assertInMultiline([
             "usage: DragonPy_CLI.py [-h]",
-            "--machine {CoCo2b,Dragon32,Dragon64,Vectrex}",
+            "--machine {CoCo2b,Dragon32,Dragon64,Multicomp6809,Vectrex}",
             "{run,editor,benchmark}",
         ], cli_out)
 
