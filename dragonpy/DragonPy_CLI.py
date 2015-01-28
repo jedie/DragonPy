14936843d472aef525e0d011ee36e8113e58b6db
JensDiemer <git@jensdiemer.de>
2015-01-28 22:20:09 +0100

    start to reimplement the CLI with "Click"

--- dragonpy/DragonPy_CLI.py
+++ dragonpy/DragonPy_CLI.py
@@ -6,7 +6,7 @@
     ~~~~~~~~~~~~~~
 
     :created: 2013 by Jens Diemer - www.jensdiemer.de
-    :copyleft: 2013-2014 by the DragonPy team, see AUTHORS for more details.
+    :copyleft: 2013-2015 by the DragonPy team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -17,6 +17,12 @@ import atexit
 import locale
 import logging
 import sys
+
+
+# https://pypi.python.org/pypi/Click/
+import click
+
+from dragonlib.utils.logging_utils import setup_logging, LOG_LEVELS
 
 from basic_editor.editor import run_basic_editor
 
@@ -41,6 +47,15 @@ from dragonpy.vectrex.machine import run_Vectrex
 
 log = logging.getLogger(__name__)
 
+
+# DEFAULT_LOG_FORMATTER = "%(message)s"
+# DEFAULT_LOG_FORMATTER = "%(processName)s/%(threadName)s %(message)s"
+# DEFAULT_LOG_FORMATTER = "[%(processName)s %(threadName)s] %(message)s"
+# DEFAULT_LOG_FORMATTER = "[%(levelname)s %(asctime)s %(module)s] %(message)s"
+# DEFAULT_LOG_FORMATTER = "%(levelname)8s %(created)f %(module)-12s %(message)s"
+DEFAULT_LOG_FORMATTER = "%(relativeCreated)-5d %(levelname)8s %(module)13s %(lineno)d %(message)s"
+
+
 machine_dict.register(configs.DRAGON32, (run_Dragon32, Dragon32Cfg), default=True)
 machine_dict.register(configs.DRAGON64, (run_Dragon64, Dragon64Cfg))
 machine_dict.register(configs.COCO2B, (run_CoCo2b, CoCo2bCfg))
@@ -60,6 +75,114 @@ def goodbye():
     print("\n --- END --- \n")
 
 
+
+class CliConfig(object):
+    def __init__(self, machine, log, verbosity, log_formatter):
+        self.machine = machine
+        self.log = log
+        self.verbosity=int(verbosity)
+        self.log_formatter=log_formatter
+
+        self.setup_logging()
+
+        self.cfg_dict = {
+            "verbosity":self.verbosity,
+            "trace":None,
+        }
+        machine_name = self.machine
+        self.machine_run_func, self.machine_cfg = machine_dict[machine]
+
+    def setup_logging(self):
+        handler = logging.StreamHandler()
+
+        # Setup root logger
+        setup_logging(
+            level=self.verbosity,
+            logger_name=None, # Use root logger
+            handler=handler,
+            log_formatter=self.log_formatter
+        )
+
+        if self.log is None:
+            return
+
+        # Setup given loggers
+        for logger_cfg in self.log:
+            logger_name, level = logger_cfg.rsplit(",", 1)
+            level = int(level)
+
+            setup_logging(
+                level=level,
+                logger_name=logger_name,
+                handler=handler,
+                log_formatter=self.log_formatter
+            )
+
+cli_config = click.make_pass_decorator(CliConfig)
+
+
+@click.group()
+@click.version_option(dragonpy.__version__)
+@click.option("--machine",
+    type=click.Choice(sorted(machine_dict.keys())),
+    default=machine_dict.DEFAULT,
+    help="Used machine configuration (Default: %s)" % machine_dict.DEFAULT)
+@click.option("--log", default=False, multiple=True,
+    help="Setup loggers, e.g.: --log DragonPy.cpu6809,50 --log dragonpy.Dragon32.MC6821_PIA,10")
+@click.option("--verbosity",
+    type=click.Choice(["%i" % level for level in LOG_LEVELS]),
+    default="%i" % logging.CRITICAL,
+    help="verbosity level to stdout (lower == more output! default: %s)" % logging.INFO)
+@click.option("--log_formatter", default=DEFAULT_LOG_FORMATTER,
+    help="see: http://docs.python.org/2/library/logging.html#logrecord-attributes")
+@click.pass_context
+def cli(ctx, **kwargs):
+    log.critical("cli kwargs: %s", repr(kwargs))
+    ctx.obj = CliConfig(**kwargs)
+
+
+@cli.command()
+@cli_config
+def editor(cli_config):
+    log.critical("Use machine cfg: %s", cli_config.machine_cfg.__name__)
+    cfg = cli_config.machine_cfg(cli_config.cfg_dict)
+    run_basic_editor(cfg)
+
+
+@cli.command()
+@click.option("--trace", default=False,
+    help="Create trace lines."
+)
+@click.option("--ram", default=None, help="RAM file to load (default none)")
+@click.option("--rom", default=None, help="ROM file to use (default set by machine configuration)")
+@click.option("--max_ops", default=None, type=int,
+    help="If given: Stop CPU after given cycles else: run forever")
+@cli_config
+def run(cli_config, **kwargs):
+    log.critical("Use machine func: %s", cli_config.machine_run_func.__name__)
+    log.critical("cli run kwargs: %s", repr(kwargs))
+    cli_config.cfg_dict.update(kwargs)
+    cli_config.machine_run_func(cli_config.cfg_dict)
+
+
+DEFAULT_LOOPS = 5
+@cli.command()
+@click.option("--loops", default=DEFAULT_LOOPS,
+    help="How many benchmark loops should be run? (default: %i)" % DEFAULT_LOOPS)
+@cli_config
+def benchmark(cli_config, loops):
+    log.critical("Run a benchmark only...")
+    run_benchmark(loops)
+
+
+if __name__ == "__main__":
+    cli()
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
 class DragonPyCLI(Base_CLI):
     LOG_NAME = "DragonPy"
     DESCRIPTION = "DragonPy - Dragon 32 emulator in Python"
@@ -135,10 +258,10 @@ class DragonPyCLI(Base_CLI):
         self.setup_logging(self.args)
 
         self.cfg_dict = {
-            "verbosity":self.args.verbosity,
+            "verbosity":self.verbosity,
             "trace":None,
         }
-        machine_name = self.args.machine
+        machine_name = self.machine
         self.machine_run_func, self.machine_cfg = self.machine_dict[machine_name]
 
 #     def open_webbrowser(self):
@@ -146,21 +269,21 @@ class DragonPyCLI(Base_CLI):
 #         webbrowser.open(url)
 
     def run(self): # Called from ../DragonPy_CLI.py
-        log.critical("run func: %s", self.args.func.__name__)
-        self.args.func()
+        log.critical("run func: %s", self.func.__name__)
+        self.func()
 
     def run_machine(self):
-#         if not self.args.dont_open_webbrowser:
+#         if not self.dont_open_webbrowser:
 #             threading.Timer(interval=1.0, function=self.open_webbrowser).start()
 
         log.critical("Use machine func: %s", self.machine_run_func.__name__)
         self.cfg_dict.update({
-            "trace":self.args.trace,
-#             "bus_socket_host":self.args.bus_socket_host,
-#             "bus_socket_port":self.args.bus_socket_port,
-            "ram":self.args.ram,
-#             "rom":self.args.rom,
-            "max_ops":self.args.max_ops,
+            "trace":self.trace,
+#             "bus_socket_host":self.bus_socket_host,
+#             "bus_socket_port":self.bus_socket_port,
+            "ram":self.ram,
+#             "rom":self.rom,
+            "max_ops":self.max_ops,
         })
         self.machine_run_func(self.cfg_dict)
 
@@ -171,12 +294,12 @@ class DragonPyCLI(Base_CLI):
 
     def run_benchmark(self):
         log.critical("Run a benchmark only...")
-        run_benchmark(self.args.loops)
+        run_benchmark(self.loops)
 
-def get_cli():
-    cli = DragonPyCLI(machine_dict)
-    cli.setup_cfg()
-    return cli
+# def get_cli():
+#     cli = DragonPyCLI(machine_dict)
+#     cli.setup_cfg()
+#     return cli
 
 
 #------------------------------------------------------------------------------
@@ -208,7 +331,7 @@ def test_run():
     subprocess.Popen(cmd_args, cwd="..").wait()
 
 
-if __name__ == "__main__":
-    print("ERROR: Use .../DragonPy/DragonPy_CLI.py instead of this file!")
+# if __name__ == "__main__":
+    # print("ERROR: Use .../DragonPy/DragonPy_CLI.py instead of this file!")
 #     test_run() # Should be only enabled for developing!
 
