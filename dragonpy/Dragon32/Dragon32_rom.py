a43a0343bf5730685375fbdd0eaf93161d150422
JensDiemer <git@jensdiemer.de>
2020-10-01 10:20:25 +0200

    Update Dragon 32/64 ROM download urls

--- dragonpy/Dragon32/Dragon32_rom.py
+++ dragonpy/Dragon32/Dragon32_rom.py
@@ -2,8 +2,11 @@
     DragonPy - 6809 emulator in Python
     ==================================
 
+    Dragon ROM Downloads from:
+    http://archive.worldofdragon.org/archive/index.php?dir=Software/Dragon/Dragon%20Data%20Ltd/Dragon%20Firmware/
+
     :created: 2015 by Jens Diemer - www.jensdiemer.de
-    :copyleft: 2015 by the DragonPy team, see AUTHORS for more details.
+    :copyleft: 2015-2020 by the DragonPy team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -13,7 +16,11 @@ from dragonpy.components.rom import ARCHIVE_EXT_ZIP, ROMFile
 
 class Dragon32Rom(ROMFile):
     ARCHIVE_EXT = ARCHIVE_EXT_ZIP
-    URL = r"http://archive.worldofdragon.org/archive/index.php?dir=Roms/Dragon/\&file=Dragon%20Data%20Ltd%20-%20Dragon%2032%20-%20IC17.zip"
+    URL = (
+        r"http://archive.worldofdragon.org/archive/index.php"
+        r"?dir=Software/Dragon/Dragon%20Data%20Ltd/Dragon%20Firmware/"
+        r"&file=Dragon%2032%20-%20IC17%26IC18%20%281982%29%28Dragon%20Data%20Ltd%29%5B%21%5D.zip"
+    )
     DOWNLOAD_SHA1 = "2cc4cbf81769746d261063eee20719899a001fed"  # downloaded .zip archive
     FILE_COUNT = 1  # How many files are in the archive?
     RENAME_DATA = {"Dragon Data Ltd - Dragon 32 - IC17.ROM": "d32.rom"}
--- dragonpy/Dragon64/Dragon64_rom.py
+++ dragonpy/Dragon64/Dragon64_rom.py
@@ -2,8 +2,14 @@
     DragonPy - 6809 emulator in Python
     ==================================
 
+    Dragon ROM Downloads from:
+    http://archive.worldofdragon.org/archive/index.php?dir=Software/Dragon/Dragon%20Data%20Ltd/Dragon%20Firmware/
+
+    But Dragon 64 ROMs currently wrong, see:
+    https://archive.worldofdragon.org/phpBB3/viewtopic.php?f=3&t=10883
+
     :created: 2015 by Jens Diemer - www.jensdiemer.de
-    :copyleft: 2015 by the DragonPy team, see AUTHORS for more details.
+    :copyleft: 2015-2020 by the DragonPy team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -13,7 +19,7 @@ from dragonpy.components.rom import ARCHIVE_EXT_ZIP, ROMFile
 
 class Dragon64RomIC17(ROMFile):
     ARCHIVE_EXT = ARCHIVE_EXT_ZIP
-    URL = r"http://archive.worldofdragon.org/archive/index.php?dir=Roms/Dragon/\&file=Dragon%20Data%20Ltd%20-%20Dragon%2064%20-%20IC17.zip"
+    URL = r"https://archive.worldofdragon.org/phpBB3/download/file.php?id=4556"
     DOWNLOAD_SHA1 = "b3e734af642d9bb6f4338352b4347764dab24383"  # downloaded .zip archive
     FILE_COUNT = 1  # How many files are in the archive?
     RENAME_DATA = {"Dragon Data Ltd - Dragon 64 - IC17.ROM": "d64_ic17.rom"}
@@ -23,7 +29,7 @@ class Dragon64RomIC17(ROMFile):
 
 class Dragon64RomIC18(ROMFile):
     ARCHIVE_EXT = ARCHIVE_EXT_ZIP
-    URL = r"http://archive.worldofdragon.org/archive/index.php?dir=Roms/Dragon/\&file=Dragon%20Data%20Ltd%20-%20Dragon%2064%20-%20IC18.zip"
+    URL = r"https://archive.worldofdragon.org/phpBB3/download/file.php?id=4557"
     DOWNLOAD_SHA1 = "bf86bfa57a4cf2a8bec23457b4b6a41d8cc7d597"  # downloaded .zip archive
     RENAME_DATA = {"Dragon Data Ltd - Dragon 64 - IC18.ROM": "d64_ic18.rom"}
     FILE_COUNT = 1  # How many files are in the archive?
