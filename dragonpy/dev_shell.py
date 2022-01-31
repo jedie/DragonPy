20d117d56fdeffb459e4815c2d1ec852425dba50
JensDiemer <git@jensdiemer.de>
2022-01-31 17:57:20 +0100

    Bugfix: Exist on CLI usage

--- dragonpy/dev_shell.py
+++ dragonpy/dev_shell.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import cmd2
 from creole.setup_utils import assert_rst_readme
-from dev_shell.base_cmd2_app import DevShellBaseApp
+from dev_shell.base_cmd2_app import DevShellBaseApp, run_cmd2_app
 from dev_shell.command_sets import DevShellBaseCommandSet
 from dev_shell.command_sets.dev_shell_commands import DevShellCommandSet as OriginDevShellCommandSet
 from dev_shell.command_sets.dev_shell_commands import run_linters
@@ -103,5 +103,5 @@ def devshell_cmdloop():
     Entry point to start the "dev-shell" cmd2 app.
     Used in: [tool.poetry.scripts]
     """
-    c = DevShellApp(**get_devshell_app_kwargs())
-    sys.exit(c.cmdloop())
+    app = DevShellApp(**get_devshell_app_kwargs())
+    run_cmd2_app(app)  # Run a cmd2 App as CLI or shell
